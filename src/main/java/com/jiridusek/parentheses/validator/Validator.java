21e853f3953b0c1368817be6f1cd8832fb55f742
Jiri Dusek <jiri.dusek@devterium.cz>
2020-09-05 00:48:18 +0200

    Validator - Javadoc added

--- src/main/java/com/jiridusek/parentheses/validator/Validator.java
+++ src/main/java/com/jiridusek/parentheses/validator/Validator.java
@@ -1,5 +1,12 @@
 package com.jiridusek.parentheses.validator;
 
+/**
+ * Validator interface
+ *
+ * @author  Jiri Dusek
+ * @version 1.0
+ * @since   2020-09-04
+ */
 public interface Validator {
 
     boolean validateParentheses(String line);
